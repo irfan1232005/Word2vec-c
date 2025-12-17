ddaf86b10ee7e7329150464d1a0e32708c12ca37
Irfan <irfanur2110@gmail.com>
2025-12-17 22:30:37 +0600

    Initial Word2Vec code added

---
+++ word2vec.c
@@ -0,0 +1,15 @@
+
+
+//word2vec from scratch
+
+#include<stdio.h>
+#include<stdlib.>
+#include<math.h>
+#include<string.h>
+#include<time.h>
+
+#define MAX_STRING 100
+#define MAX_SENTENCE_LENGTH 
+
+
+long long layer 
